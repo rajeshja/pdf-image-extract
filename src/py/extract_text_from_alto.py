29b2fc32b6a0d6e78d220e5fe6072e5b8e3fcb52
Rajesh J Advani <rajeshjadvani@yahoo.com>
2024-03-14 22:04:30 +0530

    Read all Alto files and combined into one big chunk.

--- src/py/extract_text_from_alto.py
+++ src/py/extract_text_from_alto.py
@@ -1,11 +1,25 @@
 import xml.etree.ElementTree as ET
 
+def append_content_to_line(line, content):
+    if len(line) < 2:
+        line += content
+    elif line[-2:] == "-\n" and content[0].isalpha():
+        # Fix hyphenated words
+        line = line[0:-2] + content
+    elif line[-2:] == ".\n" or (line[-2:-1] != "." and line[-1] == "\n"):
+        # Fullstop without new paragraph (unreliable?)
+        # or middle of a sentence. Remove previous newline
+        # and add next word
+        line = line[0:-1] + " " + content
+    else:
+        line += content
+    return line
+
 def parse_xml(xml_file):
     # Parse XML with ElementTree
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
-    print("About to enter loop")
     # Iterate over each "string" element in the XML
     line = ""
     for elem in root.iter():
@@ -19,21 +33,34 @@ def parse_xml(xml_file):
                 width = 1
             for i in range(width):
                 line += " "
-            # print(f'Tag: {tag}, Content: {content}, Height: {height}')
         elif tag.lower() == "string":
             content = elem.get('CONTENT')
             height = elem.get('HEIGHT')
-            if line[-2:] == "-\n" and content[0].isalpha():
-                print("Found a hyphen at the end of the line")
-                line = line[0:-2] + content
-            elif line[-2:] == ".\n":
-                line = line[0:-1] + " " + content
-            elif line[-2:-1] != "." and line[-1] == "\n":
-                line = line[0:-1] + " " + content
-            else:
-                line += content
+            line = append_content_to_line(line, content)
 
-    print(line)
+    return line
 
 # Call the function with your XML file path
-parse_xml('C:/temp/dnd-rulebooks/images/cleaned_images/xml-nocontours/image6_1.xml')
+parse_xml('C:/temp/dnd-rulebooks/images/cleaned_images/xml-nocontours/image8_1.xml')
+
+import os, glob
+
+alto_directory = "c:/temp/dnd-rulebooks/images/cleaned_images/xml-nocontours"
+text_directory = os.path.join(alto_directory, "alltext")
+
+if not os.path.exists(alto_directory):
+    print(f"Directory does not exist: {alto_directory}")
+
+xmls = glob.glob(os.path.join(alto_directory, '*.xml'))
+
+all_text = ""
+
+for xml in sorted(xmls, key=lambda name: int(name.split(os.path.sep)[-1][5:-6])):
+    all_text =  append_content_to_line(all_text, parse_xml(xml))
+
+if not os.path.exists(text_directory):
+    os.makedirs(text_directory)
+
+with open(os.path.join(text_directory, "alltext.txt"), 'w') as file:
+    file.write(all_text)
+
